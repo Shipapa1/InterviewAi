03ce43058520acc92349267b0c1c2786bf195828
Nathan Nguyen <nathanduynguyen@Nathans-MacBook-Pro.local>
2025-07-29 12:36:41 -0600

    Sign in page compelted

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,67 +1,114 @@
 "use client"
- 
+
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
+import Link from "next/link"
 import Image from "next/image"
-import { toast } from "sonner";
+import { toast, Toaster } from "sonner"
+import { useRouter } from "next/navigation"
 
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-})
-
+import { Form } from "@/components/ui/form"
 import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+import FormField from "./FormField"
 
+const authFormSchema = (type: FormType) =>
+  z.object({
+    name: type === "sign-up" ? z.string().min(3) : z.string().optional(),
+    email: z.string().email(),
+    password: z.string().min(3),
+  })
 
-const AuthForm = ({type}: { type: FormType}) => {
-      // 1. Define your form.
+const AuthForm = ({ type }: { type: FormType }) => {
+  const router = useRouter()
+  const formSchema = authFormSchema(type)
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      name: "",
+      email: "",
+      password: "",
     },
   })
- 
-  // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      if (type === "sign-up") {
+        await new Promise((res) => setTimeout(res, 500))
+        toast.success("Account created successfully. Please sign in!");
+        router.push("/sign-in");
+      } else {
+        await new Promise((res) => setTimeout(res, 500))
+        toast.success("Signed in successfully!");
+        router.push("/");
+      }
+    } catch (error: any) {
+      console.error(error);
+      toast.error(`There was an error: ${error.message || error}`)
+    }
   }
 
-  const isSignIn = type === 'sign-in';
-
+  const isSignIn = type === "sign-in"
 
   return (
     <div className="card-border lg:min-w-[566px]">
+      {/* 🟡 Add toaster here */}
+      <Toaster richColors position="top-center" />
+
       <div className="flex flex-col gap-6 card py-14 px-10">
         <div className="flex flex-row gap-2 justify-center">
           <Image src="/logo.svg" alt="logo" height={32} width={38} />
           <h2 className="text-primary-100">PrepWise</h2>
-            </div>
-            <h3>Practice job interviews with AI</h3>
-      
-        
-            <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 mt-4 form">
-               {!isSignIn && <p>Name</p>}
-               <p>Email</p>
-               <p>Password</p>
-        <Button className = "btn" type="submit">{isSignIn ? 'Sign in' : 'Create an Account' }</Button>
-      </form>
-    </Form></div>
+        </div>
+        <h3>Practice job interviews with AI</h3>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 mt-4 form">
+            {!isSignIn && (
+              <FormField
+                control={form.control}
+                name="name"
+                label="Name"
+                placeholder="Your Name"
+                type="text"
+              />
+            )}
+            <FormField
+              control={form.control}
+              name="email"
+              label="Email"
+              placeholder="Your email address"
+              type="email"
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              label="Password"
+              placeholder="Enter your password"
+              type="password"
+            />
+            <Button className="btn" type="submit">
+              {isSignIn ? "Sign in" : "Create an Account"}
+            </Button>
+          </form>
+        </Form>
+
+        <p className="text-center">
+          {isSignIn ? "No account yet?" : "Have an account already?"}
+          <Link
+            href={!isSignIn ? "/sign-in" : "/sign-up"}
+            className="font-bold text-user-primary ml-1"
+          >
+            {!isSignIn ? "Sign in" : "Sign up"}
+          </Link>
+        </p>
       </div>
+    </div>
   )
 }
 
 export default AuthForm
+
+
+    
--- components/FormField.tsx
+++ components/FormField.tsx
@@ -9,24 +9,38 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 
+interface FormFieldProps<T extends FieldValues>{
+  control: Control<T>;
+  name: Path<T>;
+  label: string;
+  placeholder?: string;
+  type?: 'text' | 'email' | 'password' | 'file'
+}
 
-const FormField = () => (
-            <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
+
+const FormField = ({control, 
+  name, 
+  label,
+  placeholder, 
+  type = "text",
+ }: FormFieldProps<T>) => {
+        return (
+           <Controller
+            name = {name} 
+           control = {control}
+            render = {({ field }) => (
             <FormItem>
-              <FormLabel>Username</FormLabel>
+              <FormLabel className = "label">{label}</FormLabel>
               <FormControl>
-                <Input placeholder="shadcn" {...field} />
+                <Input className ="input" placeholder= {placeholder}
+                type = {type}
+                {...field} />
               </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
 );
+};
 
 export default FormField
